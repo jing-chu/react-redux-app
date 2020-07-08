218ea8e45db35e42fbcd0d04c02e12e6dc930a53
jingjincs <jinjingcs@gmail.com>
2020-07-08 15:38:17 +0200

    react-redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom';
+import Header from './Header'
+import Content from './Content'
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function createStore (reducer) {
+  let state = null
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+  dispatch({}) // 初始化 state
+  return { getState, dispatch, subscribe }
+}
+
+const themeReducer = (state, action) => {
+  if (!state) return {
+    themeColor: 'red'
+  }
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, themeColor: action.themeColor }
+    default:
+      return state
+  }
+}
+
+const store = createStore(themeReducer)
+
+
+class Index extends Component {
+  static childContextTypes = {
+    store: PropTypes.object
+  }
+
+
+  getChildContext () {
+    return { store  }
+  }
+
+  render () {
+    return (
+      <div>
+        <Header />
+        <Content />
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Index />,
   document.getElementById('root')
 );
 
